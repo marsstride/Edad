bfdd9b3e124d23f8257116b39675f5eed95edda2
marsstride <mariavidaurrealvarado@gmail.com>
2025-06-12 04:46:38 +0000

    codigo listas

--- main.py
+++ main.py
@@ -65,3 +65,73 @@ while contador < numero:  # la condición depende del número ingresado
     print(f"Contador es: {contador}")
     contador = contador + 1  # Actualiza la variable de control(contador)
 print("¡El bucle while ha terminado!")
+
+
+
+# Código del juego adivina el número secreto
+# El número secreto ahora es fijo: 6 pero puede ser cambiado por cualquier otro número entero.
+numero_secreto = 6
+
+print("¡Bienvenido al juego Adivina el Número Secreto!")
+print("Estoy pensando en el...")
+
+# Iniciamos la variable adivinanza con un valor diferente al número secreto para que el bucle WHILE se ejecute al menos una vez.
+adivinanza = 0 
+
+# Bucle que continua mientras la adivinanza no sea correcta
+while adivinanza != numero_secreto:
+    try:
+        # Pide al usuario que adivine el número.
+        adivinanza = int(input("Ingresa el número que crees que es el secreto: "))
+
+        # Aqui le damos una pista si el número no es correcto
+        if adivinanza > numero_secreto:
+            print("Demasiado alto. Intenta de nuevo.")
+        elif adivinanza < numero_secreto:
+            print("Demasiado bajo. Intenta de nuevo.")
+
+    except ValueError:
+        # Maneja el error si el usuario no ingresa un número entero (int)
+        print("Ingresa un número entero.")
+
+# Mensaje (print)que aparece cuando el número fue adivinado
+print(f"¡Correcto! El número era {numero_secreto}. ¡Felicidades, adivinaste!")
+
+
+
+#Codigo lista de comidas bolivianas favoritas
+ # Crea una lista llamada comidas_favoritas con los nombres (strings) de tus 3 comidas bolivianas favoritas.
+comidas_favoritas = ["Pollo Frito", "Pique Macho", "Milanesa"]
+
+# Imprimimos la lista completa para verificar que se creó bien.
+print("Mi lista de comidas favoritas es:")
+print(comidas_favoritas)
+
+# Imprime un mensaje que diga: "Mi segunda comida favorita es: [el nombre de la segunda comida]".
+print(f"\nMi segunda comida favorita es: {comidas_favoritas[1]}")
+
+# Usamos len() al imprimir un mensaje 
+# len() nos dara la cantidad de elementos en la lista.
+print(f"Mi lista de comidas favoritas tiene {len(comidas_favoritas)} elementos.")
+
+
+
+#Codigo para calcular el promedio de notas
+# Creamos una lista llamada lista_mis_notas con las notas numéricas.
+lista_mis_notas = [60.5, 74, 80.5, 90.5,]
+# Creamos una variable suma_total inicializada en 0.
+suma_total = 0
+
+# Usamos un bucle for para recorrer lista_mis_notas.
+# En cada iteración, añade la nota actual a suma_total.
+for nota in lista_mis_notas:
+    suma_total = suma_total + nota 
+
+# Calcula el promedio: promedio = suma_total / len(lista_mis_notas).
+# len(lista_mis_notas) nos da la cantidad de elementos en la lista.
+promedio = suma_total / len(lista_mis_notas)
+
+# Imprime(print) la suma total y el promedio.
+print(f"Mis notas son: {lista_mis_notas}")
+print(f"La suma total de las notas es: {suma_total:.2f}") 
+print(f"El promedio de las notas es: {promedio:.2f}") # :.2f para mostrar dos decimales
